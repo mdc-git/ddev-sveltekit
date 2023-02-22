a12546a8fcd7b57100be1ab01f9f708c686e0789
mdc-git <65224188+mdc-git@users.noreply.github.com>
2023-02-22 21:57:07 +0100

    Update hooks.server.js

--- sveltekit/hooks.server.js
+++ sveltekit/hooks.server.js
@@ -5,6 +5,7 @@ export async function handle({ event, resolve }) {
         event.url = new URL(event.url.href.replace('http://node-srv', process.env.DDEV_PRIMARY_URL))
     }
     const response = await resolve(event);
+    console.log('src/hooks.server.js: ' + event.url.pathname + ' ' + response.status);
     return response;
 }
 
